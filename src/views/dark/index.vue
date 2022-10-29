59f61216b6c781ddf4084b90608e142a1cca9378
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-10-29 16:01:06 +0100

    pushed to github;

--- src/views/dark/index.vue
+++ src/views/dark/index.vue
@@ -3,7 +3,6 @@
     <Navigation />
     <Hero2 />
     <section>
-      <Tweet/>
       <Achievments />
       <Portfolio />
       <Testimonials />
@@ -15,7 +14,6 @@
 <script>
 import Navigation from "./Navigation.vue";
 import Hero2 from "./Hero2.vue";
-import Tweet from "./Tweet.vue";
 import Portfolio from "./portfolio.vue";
 import Testimonials from "./testimonials.vue";
 import Footer from "./footer.vue";
@@ -25,7 +23,6 @@ export default {
   components: {
     Navigation,
     Hero2,
-    Tweet,
     Portfolio,
     Testimonials,
     Achievments,
