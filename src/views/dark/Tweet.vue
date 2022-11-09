ecd48beda34c6365ac8ea0c135bbddbbc5f924e5
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-11-09 01:16:45 +0100

    removed tweet

--- src/views/dark/Tweet.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <a class="twitter-timeline" data-height="600" data-theme="dark" href="https://twitter.com/solomonmarvel97?ref_src=twsrc%5Etfw">Tweets by solomonmarvel97</a> 
-    </div>
-</template>
