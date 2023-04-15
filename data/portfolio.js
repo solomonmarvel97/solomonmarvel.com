985e93314828326be235c35bd84d63d74d7475f2
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-04-15 13:39:36 +0100

    changed the positions to 'founding partner'

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,6 +1,6 @@
 exports.portfolio = [
   {
-    position: `Founder & CEO`,
+    position: `Founding Partner`,
     platform: `Top Universe`,
     link: `https://topuniverse.org`,
     image: `https://topuniverse.org/ogimage.png`
@@ -18,19 +18,19 @@ exports.portfolio = [
     image: `https://betakopa.com/ogimage.png`
   },
   {
-    position: `Founder & Chief Solutions Architect`,
+    position: `Founding Partner`,
     platform: `The Boltcliq Company`,
     link: `https://boltcliq.com`,
     image: `https://images.unsplash.com/photo-1655721533500-c73a495dbcb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80`
   },
   {
-    position: `Founder & CTO`,
+    position: `Founding Partner`,
     platform: `Kids Code Club`,
     link: `https://kidsclub.codes`,
     image: `https://kidsclub.codes/ogimage.png`
   },
   {
-    position: `Founder & Cloud Trainer`,
+    position: `Founding Partner`,
     platform: `Techies Garage`,
     link: `https://techiesgarage.org`,
     image: `https://techiesgarage.org/ogimage.png`
