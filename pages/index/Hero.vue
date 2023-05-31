e56769f88b11a9e23b579197fa0aabf8a3cc9697
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-05-31 23:32:17 +0100

    Removed duplicate description

--- pages/index/Hero.vue
+++ pages/index/Hero.vue
@@ -22,7 +22,7 @@
           </a>
         </div>
         <h1 class=""mb-3">Marvelous <br> Solomon</h1>
-        <p class="mb-6">Cloud Solutions Architect | Software/AI/ML/NLP Engineer</p>
+        <p class="mb-6">Solutions Architect (Cloud & Data) | Software/AI/ML/NLP Engineer</p>
 
         <div class="button-flex mb-3 cta">
           <a href="/consulting">
