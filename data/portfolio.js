7f453f2e3fe21c6b1108fd7e441f4301cd34bd7f
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-07-03 04:00:29 +0100

    added swiftgo.co ogimage

--- data/portfolio.js
+++ data/portfolio.js
@@ -39,6 +39,6 @@ exports.portfolio = [
     position: `Founding Partner`,
     platform: `Swiftgo`,
     link: `https://swiftgo.co`,
-    image: `https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80`
+    image: `https://swiftgo.co/ogimage.png`
   },
 ];
