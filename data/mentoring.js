87cd1d1631debce706bf161d749e63da0d67270e
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-07-13 18:52:07 +0100

    changed whatsapp group link to telegram

--- data/mentoring.js
+++ data/mentoring.js
@@ -1,55 +1,55 @@
 // import image1 from '@/assets/images/mentoring/1.png'
 
 exports.mentoring = [
-    {
-        id: '851d64af-8393-49c3-a258-6935a0fcf556',
-        image: require('@/assets/images/mentoring/1.png'),
-        title: 'Software Engineering & Cloud Mentoring Group',
-        description: `If you are a software engineer or cloud engineer who is dedicated and motivated, eager to learn, open to feedback and you're in need of a mentor, I encourage you to reach out to me. I would be happy to discuss your goals and see if we are a good fit for each other`,
-        link: "https://chat.whatsapp.com/BgwVNSvOEpWK4iblTLGv5i",
-        details: [
-            {
-                title: 'Guidance and advice on career development'
-            },
-            {
-                title: 'Support and encouragement'
-            },
-            {
-                title: 'Learn new skills and technologies'
-            },
-            {
-                title: 'Learn modern toolings'
-            },
-            {
-                title: 'Learn best practices'
-            },
-            {
-                title: 'Receive learning resouces/guides'
-            },
-            {
-                title: 'Network with the community'
-            },
-            {
-                title: 'Become an industry professional',
-            },
-            {
-                title: 'Learn software engineering'
-            },
-            {
-                title: 'Learn system design'
-            },
-            {
-                title: 'Learn software architecture'
-            },
-            {
-                title: 'SE book reviews'
-            },
-            {
-                title: 'Software engineering newsletter trends'
-            },
-            {
-                title: 'Learn cloud computing, platforms & services'
-            },
-        ]
-    },
+  {
+    id: '851d64af-8393-49c3-a258-6935a0fcf556',
+    image: require('@/assets/images/mentoring/1.png'),
+    title: 'Software Engineering & Cloud Mentoring Group',
+    description: `If you are a software engineer or cloud engineer who is dedicated and motivated, eager to learn, open to feedback and you're in need of a mentor, I encourage you to reach out to me. I would be happy to discuss your goals and see if we are a good fit for each other`,
+    link: "https://t.me/+_vyGNhGhhSA2ZDU0",
+    details: [
+      {
+        title: 'Guidance and advice on career development'
+      },
+      {
+        title: 'Support and encouragement'
+      },
+      {
+        title: 'Learn new skills and technologies'
+      },
+      {
+        title: 'Learn modern toolings'
+      },
+      {
+        title: 'Learn best practices'
+      },
+      {
+        title: 'Receive learning resouces/guides'
+      },
+      {
+        title: 'Network with the community'
+      },
+      {
+        title: 'Become an industry professional',
+      },
+      {
+        title: 'Learn software engineering'
+      },
+      {
+        title: 'Learn system design'
+      },
+      {
+        title: 'Learn software architecture'
+      },
+      {
+        title: 'SE book reviews'
+      },
+      {
+        title: 'Software engineering newsletter trends'
+      },
+      {
+        title: 'Learn cloud computing, platforms & services'
+      },
+    ]
+  },
 ]
