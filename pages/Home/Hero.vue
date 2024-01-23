1c355feb6dbd3a1b0729925e0f7f1d1af7d0283a
Marvelous Solomon <solomonmarvel@hotmail.com>
2024-01-23 08:34:01 +0100

    added some sample codes to the main.js file

--- pages/Home/Hero.vue
+++ pages/Home/Hero.vue
@@ -50,9 +50,12 @@
 
         </section>
         <div class="center">
-            <iframe src="https://player.twitch.tv/?channel=solomonmarvel&parent=https://solomonmarvel.com" frameborder="0"
+            <iframe src="https://player.twitch.tv/?channel=solomonmarvel&parent=www.solomonmarvel.com" frameborder="0"
                 scrolling="no" allowfullscreen="true" allow="autoplay; fullscreen"
                 class="mb-5 custom-twitch-iframe"></iframe>
+            <!-- <iframe src="https://player.twitch.tv/?video=2035415438&parent=solomonmarvel.com" frameborder="0"
+                allowfullscreen="true" scrolling="no" height="378" width="620"></iframe> -->
+
         </div>
 
     </div>
